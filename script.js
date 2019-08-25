888cea97b28852913c07eb3f88369917c7af13e5
Logan <logan@brix.ninja>
2019-08-25 16:17:37 -0500

    a

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const review = document.querySelector("#review");
+const submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", () => {
+	alert("Thanks for the report");
+	review.value = "";
+});
 
-    
-
+const stars = Array.from(document.querySelectorAll(".star"));
+stars.forEach((star, i) => {
+	star.addEventListener("change", () => {
+		stars.forEach((s, j) => {
+			s.checked = j <= i;
+		});
+	});
+});
