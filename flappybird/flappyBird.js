8e2d2c2c002db156e057500e50588a4acf13718d
Logan <logan@brix.ninja>
2019-10-01 15:32:03 -0500

    a

--- flappybird/flappyBird.js
+++ flappybird/flappyBird.js
@@ -1,9 +1,10 @@
-var cvs = document.getElementById("canvas");
+var cvs = document.querySelector("#canvas");
+cvs.width = window.innerWidth;
 var ctx = cvs.getContext("2d");
 
 // load images
 
-var bird = new Image();
+var bird = new Image()
 var bg = new Image();
 var fg = new Image();
 var pipeNorth = new Image();
@@ -15,18 +16,23 @@ fg.src = "images/fg.png";
 pipeNorth.src = "images/pipeNorth.png";
 pipeSouth.src = "images/pipeSouth.png";
 
-
 // some variables
 
-var gap = 85;
+var gap = 100;
 var constant;
+var frames = 0;
 
 var bX = 10;
 var bY = 150;
 
-var gravity = 1.5;
+var gravity = 2;
+var speed = 2;
+var distance = 250;
 
 var score = 0;
+
+var paused = true;
+var died = false;
 
 // audio files
 
@@ -38,11 +44,31 @@ scor.src = "sounds/score.mp3";
 
 // on key down
 
-document.addEventListener("keydown",moveUp);
+document.addEventListener("keydown", moveUp);
+document.addEventListener("click", pauseGame);
 
-function moveUp(){
+function moveUp() {
+    if (paused) {
+        paused = false;
+        draw();
+        return;
+    }
+    if (died) {
+        location.reload();
+    }
     bY -= 42;
     fly.play();
+}
+
+function pauseGame() {
+    if (died) {
+        location.reload();
+    }
+    paused = !paused;
+
+    if (!paused) {
+        draw();
+    }
 }
 
 // pipe coordinates
@@ -50,81 +76,76 @@ function moveUp(){
 var pipe = [];
 
 pipe[0] = {
-    x : cvs.width,
-    y : 0
+    x: cvs.width,
+    y: 0
 };
 
 // draw images
 
-function draw(){
-    
-    ctx.drawImage(bg,0,0);
-    
-    
-    for(var i = 0; i < pipe.length; i++){
-        
-        constant = pipeNorth.height+gap;
-        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
-        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
-             
-        pipe[i].x--;
-        
-        if( pipe[i].x == 125 ){
+function draw() {
+    frames++;
+
+    bY += gravity;
+
+    for (var i = 0; i < cvs.width; i += 287) {
+        ctx.drawImage(bg, i, 0);
+    }
+
+
+    for (var i = 0; i < pipe.length; i++) {
+
+        constant = pipeNorth.height + gap;
+        ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
+
+        pipe[i].x -= speed;
+
+        if (pipe[i].x <= cvs.width - distance && pipe[i].x > cvs.width - distance - speed) {
             pipe.push({
-                x : cvs.width,
-                y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
-            }); 
+                x: cvs.width,
+                y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+            });
         }
 
         // detect collision
-        
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
-            location.reload(); // reload the page
+
+        if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + constant) || bY + bird.height >= cvs.height - fg.height) {
+            died = true;
+            // location.reload(); // reload the page
         }
-        
-        if(pipe[i].x == 5){
+
+        if (pipe[i].x < -pipeNorth.width + 5) {
+            pipe.shift();
+            i--;
             score++;
             scor.play();
         }
-        
-        
+
+
     }
 
-    ctx.drawImage(fg,0,cvs.height - fg.height);
-    
-    ctx.drawImage(bird,bX,bY);
-    
-    bY += gravity;
-    
+    for (var i = 0; i < cvs.width; i += 287) {
+        ctx.drawImage(fg, i, cvs.height - fg.height);
+    }
+
+    ctx.drawImage(bird, bX, bY);
+
     ctx.fillStyle = "#000";
     ctx.font = "20px Verdana";
-    ctx.fillText("Score : "+score,10,cvs.height-20);
-    
-    requestAnimationFrame(draw);
-    
+    ctx.fillText("Score : " + score, 10, cvs.height - 20);
+
+    if (!paused && !died) {
+        start = false;
+        requestAnimationFrame(draw);
+    }
+
 }
 
-draw();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var imagesLoaded = 0;
+[bird, bg, fg, pipeNorth, pipeSouth].forEach(img => {
+    img.addEventListener('load', () => {
+        if (++imagesLoaded === 5) {
+            draw();
+        }
+    }, false);
+});
